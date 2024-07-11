6b04a219c796da0a6bbed6d25b3a225cd0f68480
Minkhantthwin <minkhantthwin17@gmail.com>
2024-07-11 10:31:26 +0630

    comment-box add

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -21,29 +21,7 @@
         </div>
       </div>
     </div>
-    <!--Comment Box Start -->
-    <section class="container">
-            <div class="col-md-8 mx-auto">
-            @auth
-            <x-card-wrapper>
-            <form action="/blogs/{{$blog->slug}}/comments" method="POST">
-              @csrf
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Comment Here</label>
-              <textarea class="form-control" rows="5" name="body" id=""></textarea>
-              @error('body')
-                    <p class="text-danger">{{$message}}</p>
-              @enderror
-            </div>
-           <div class="d-flex justify-content-end"><button type="submit" class="btn btn-primary">Submit</button></div>
-            
-          </form> 
-            </x-card-wrapper>
-            @endauth            
-            </div>
-      </section>
-    <!--Comment Box End -->
-    
+            <x-comment-box :blog="$blog" />
             <x-comments :comments="$blog->comments"/> 
             <x-subscribe/>
             <x-blog_you_may_like :randomBlogs="$randomBlogs"/>
---
+++ resources/views/components/comment-box.blade.php
@@ -0,0 +1,22 @@
+@props(['blog'])
+
+<section class="container">
+    <div class="col-md-8 mx-auto">
+    @auth
+    <x-card-wrapper>
+    <form action="/blogs/{{$blog->slug}}/comments" method="POST">
+      @csrf
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Comment Here</label>
+      <textarea class="form-control" rows="5" name="body" id=""></textarea>
+      @error('body')
+            <p class="text-danger">{{$message}}</p>
+      @enderror
+    </div>
+   <div class="d-flex justify-content-end"><button type="submit" class="btn btn-primary">Submit</button></div>
+    
+  </form> 
+    </x-card-wrapper>
+    @endauth            
+    </div>
+</section>
